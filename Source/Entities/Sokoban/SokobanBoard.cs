e729689b291b4fe59912448c2281ae4a7791c05e
xestevaogspx@gmail.com <xestevaogspx@gmail.com>
2026-07-25 11:00:33 -0400

    yeah sokoban time

---
+++ Source/Entities/Sokoban/SokobanBoard.cs
@@ -0,0 +1,90 @@
+﻿using Celeste.Mod.Roslyn.ModLifecycleAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Celeste.Mod.FemtoHelper.Entities.Sokoban;
+
+[CustomEntity("FemtoHelper/SokobanBoard")]
+public class SokobanBoard : Entity
+{
+    internal static VirtualRenderTarget buffer;
+    public List<SokobanObject> Objects = [];
+    private Coroutine sequence;
+    public readonly string Room;
+    public int boardWidth, boardHeight;
+    public SokobanBoard(EntityData data, Vector2 offset) : base(data.Position + offset)
+    {
+        Add(sequence = new(Sequence()));
+        Add(new BeforeRenderHook(BeforeRender));
+        Room = data.String("room");
+    }
+
+    public override void Awake(Scene scene)
+    {
+        base.Awake(scene);
+        Init(Room);
+    }
+
+    public void Init(string room)
+    {
+        LevelData data = (Scene as Level).Session.MapData.Get(room);
+        if(data == null)
+        {
+            Warn($"SokobanBoard: Could not find room '{Room}'!");
+            Add(new Text(Draw.DefaultFont, $"invalid room '{Room}'", Vector2.Zero, Color.Aquamarine));
+            return;
+        }
+
+        boardWidth = data.TileBounds.Width;
+        boardHeight = data.TileBounds.Height;
+
+        foreach(EntityData d in data.Entities.Where(d => d.Name == "FemtoHelper/SokobanObject"))
+        {
+            Objects.Add(new SokobanObject()
+            {
+                stepPercent = 0f,
+                pos = d.Position,
+                visualPos = d.Position,
+                type = d.Attr("type"),
+                prevType = d.Attr("type"),
+            });
+        }
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public void BeforeRender()
+    {
+        foreach (SokobanObject obj in Objects)
+        {
+            obj.Render();
+        }
+    }
+
+    public override void Render()
+    {
+        base.Render();
+
+    }
+
+    public IEnumerator Sequence()
+    {
+        yield return null;
+    }
+
+    [OnLoadContent]
+    public static void OnLoadContent(bool firstLoad)
+    {
+        buffer ??= VirtualContent.CreateRenderTarget("femtohelper-sokoban", 320, 180);
+    }
+
+    [OnUnload]
+    public static void Unload()
+    {
+        buffer.Dispose();
+    }
+}
---
+++ Source/Entities/Sokoban/SokobanObject.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.FemtoHelper.Entities.Sokoban;
+
+public struct SokobanObjectType
+{
+    public string SourcePath;
+    public Rectangle SourceRect;
+    public bool IsSolid;
+    public bool IsPushable;
+    public bool IsPullable;
+    public int Depth;
+}
+
+public class SokobanObject
+{
+    static SokobanObject()
+    {
+        types.Add("Player", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(0, 0, 8, 8),
+            IsSolid = false,
+            IsPushable = false,
+            IsPullable = false,
+            Depth = 0,
+        });
+        types.Add("Rock", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(8, 0, 8, 8),
+            IsSolid = true,
+            IsPushable = true,
+            IsPullable = false,
+            Depth = 100,
+        });
+        types.Add("Gem", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(16, 0, 8, 8),
+            IsSolid = true,
+            IsPushable = false,
+            IsPullable = true,
+            Depth = 150,
+        });
+        types.Add("Slot", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(24, 0, 8, 8),
+            IsSolid = false,
+            IsPushable = false,
+            IsPullable = false,
+            Depth = 600,
+        });
+        types.Add("Swap", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(32, 0, 8, 8),
+            IsSolid = false,
+            IsPushable = false,
+            IsPullable = false,
+            Depth = 400,
+        });
+        types.Add("Wall", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(48, 0, 8, 8),
+            IsSolid = true,
+            IsPushable = false,
+            IsPullable = false,
+            Depth = 200,
+        });
+        types.Add("Tile", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(40, 0, 8, 8),
+            IsSolid = false,
+            IsPushable = false,
+            IsPullable = false,
+            Depth = 300,
+        });
+        types.Add("Bomb", new SokobanObjectType
+        {
+            SourcePath = "objects/FemtoHelper/sokoban/atlas",
+            SourceRect = new Rectangle(0, 8, 8, 14),
+            IsSolid = true,
+            IsPushable = true,
+            IsPullable = false,
+            Depth = 50,
+        });
+    }
+    private static List<string> registeredTypes =
+    [
+        "Player",
+        "Rock",
+        "Gem",
+        "Slot",
+        "Swap",
+        "Wall",
+        "Tile",
+        "Bomb",
+    ];
+    private static Dictionary<string, SokobanObjectType> types = [];
+    public float stepPercent;
+    public Vector2 pos;
+    public Vector2 visualPos;
+    public string type;
+    public string prevType;
+
+    public void Render()
+    {
+        SokobanObjectType type = types[this.type];
+        GFX.Game[type.SourcePath].GetSubtexture(type.SourceRect).DrawCentered(visualPos);
+    }
+}
